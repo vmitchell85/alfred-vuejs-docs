47549f60ce5f83d20ac01f926097f63a6e3db1f3
Vince Mitchell <vincent.mitchell@gmail.com>
2018-07-31 13:05:24 -0400

    improves hierarchy display text

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 An ultra-fast VueJS docs search workflow for Alfred 3.
 
-Adapted from [Alfred Laravel Docs](https://github.com/tillkruss/alfred-laravel-docs), Thanks [Till Krüss](https://github.com/tillkruss)!
+Adapted from [Alfred Laravel Docs](https://github.com/tillkruss/alfred-laravel-docs), Thanks [Till Krüss](https://twitter.com/tillkruss)!
 
 ![Screenshot](screenshot.png)
 
 ## Installation
 
-1. [Download the latest version](https://github.com/vmitchell85/alfred-vuejs-docs/releases/download/0.1/VueJS.Docs.alfredworkflow)
+1. [Download the latest version](https://github.com/vmitchell85/alfred-vuejs-docs/releases/download/0.1.1/VueJS.Docs.alfredworkflow)
 2. Install the workflow by double-clicking the `.alfredworkflow` file
 3. You can add the workflow to a category, then click "Import" to finish importing. You'll now see the workflow listed in the left sidebar of your Workflows preferences pane.
 
--- vuejs.php
+++ vuejs.php
@@ -7,12 +7,11 @@ use AlgoliaSearch\Version as AlgoliaUserAgent;
 require __DIR__ . '/vendor/autoload.php';
 
 $query = $argv[1];
-$branch = empty($_ENV['branch']) ? 'master' : $_ENV['branch'];
 
 $workflow = new Workflow;
 $algolia = new Algolia('BH4D9OD16A', '85cc3221c9f23bfbaa4e3913dd7625ea');
 
-AlgoliaUserAgent::addSuffixUserAgentSegment('Alfred Workflow', '0.2.1');
+AlgoliaUserAgent::addSuffixUserAgentSegment('VueJS Alfred Workflow', '0.1.1');
 
 $index = $algolia->initIndex('vuejs');
 $search = $index->search($query, ['facetFilters' => 'version:v2']);
@@ -22,7 +21,7 @@ if (empty($results)) {
     $workflow->result()
         ->title('No matches')
         ->icon('google.png')
-        ->subtitle("No match found in the docs. Search Google for: \"{$query}\"")
+        ->subtitle("No match found in the docs. Search Google for: \"VueJS+{$query}\"")
         ->arg("https://www.google.com/search?q=vuejs+{$query}")
         ->quicklookurl("https://www.google.com/search?q=vuejs+{$query}")
         ->valid(true);
@@ -32,23 +31,25 @@ if (empty($results)) {
 }
 
 foreach ($results as $hit) {
-    $hasText = isset($hit['_highlightResult']['content']['value']);
-    $hasSubtitle = isset($hit['h2']);
+    $highestLvl = $hit['hierarchy']['lvl6'] ? 6 : (
+        $hit['hierarchy']['lvl5'] ? 5 : (
+            $hit['hierarchy']['lvl4'] ? 4 : (
+                $hit['hierarchy']['lvl3'] ? 3 : (
+                    $hit['hierarchy']['lvl2'] ? 2 : (
+                        $hit['hierarchy']['lvl1'] ? 1 : 0
+                    )
+                )
+            )
+        )
+    );
 
-    $title = $hit['h1'];
-    $subtitle = $hasSubtitle ? $hit['h2'] : null;
-
-    if ($hasText) {
-        $subtitle = $hit['_highlightResult']['content']['value'];
-
-        if ($hasSubtitle) {
-            $title = "{$title} » {$hit['h2']}";
-        }
+    $title = $hit['hierarchy']['lvl' . $highestLvl];
+    $currentLvl = 0;
+    $subtitle = $hit['hierarchy']['lvl0'];
+    while ($currentLvl < $highestLvl) {
+        $currentLvl = $currentLvl + 1;
+        $subtitle = $subtitle . ' » ' . $hit['hierarchy']['lvl' . $currentLvl];
     }
-
-    $title = $hit['hierarchy']['lvl6'] ?: $hit['hierarchy']['lvl5'] ?: $hit['hierarchy']['lvl4'] ?: $hit['hierarchy']['lvl2'] ?: $hit['hierarchy']['lvl1'] ?: $hit['hierarchy']['lvl0'];
-
-    $subtitle = $hit['hierarchy']['lvl0'] . ($hit['hierarchy']['lvl1'] ? ' => ' . $hit['hierarchy']['lvl1'] : '');
 
     $workflow->result()
         ->uid($hit['objectID'])
